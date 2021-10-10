9aa7940e0f74b8bdf22df9b7a8209394bc259f6f
Hussnain Raza ⚡ <hussnain444all@gmail.com>
2021-10-10 22:19:39 +0500

    first commit

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const express = require ('express')
+const morgan = require('morgan')
 const app = express()
+
+app.use(express.json())
+
+morgan.token('req_name', function (req, res) { return req.body.name })
+morgan.token('req_num', function (req,res) {return req.body.number})
+app.use(morgan(':req_name :req_num'))
+
+
 
     let persons = [
       {
@@ -21,13 +30,84 @@ const app = express()
         "name": "Albert Einstein",
         "number": "56921-1231-12",
         "id": 4
+      },
+      {
+        "name": "Waris Ali",
+        "number": "12345-3456-8",
+        "id": 5
+      },
+      {
+        "name": "Kashif Raza",
+        "number": "03065648475",
+        "id": 6
       }
     ]
 
+const count = (persons) =>
+{
+    return persons.length
+}
+
+app.get('/',(request,response)=>
+{
+    response.json('hello')
+})
 
 app.get('/api/persons',(request,response)=>
 {
     response.json(persons)
+})
+
+app.get('/api/persons/:id',(request,response) => 
+{
+  const id = Number(request.params.id)
+  const personFound = persons.find(person => person.id === id)
+  if(!personFound)
+  {
+    response.status(404).end()
+  }
+  response.json(personFound)
+})
+
+app.post('/api/persons/',(request,response)=>
+{
+  const body = request.body
+  if (!body.name || !body.number) {
+    return response.status(400).json({ 
+      error: 'some info is missing' 
+    })
+  }
+
+  const newPerson =
+  {
+    name: body.name,
+    number: body.number,
+    id: generateId()
+  }
+  persons = persons.concat(newPerson)
+  response.json(persons)
+})
+
+const generateId = () => {
+  const maxId = persons.length > 0
+    ? Math.max(...persons.map(p => p.id))
+    : 0
+  return maxId + 1
+}
+
+app.get('/info',(request,response)=>
+{
+    const total = count(persons)
+    str = `Phonebook has info of ${total} people`
+    const date = Date()
+    const arr = [str,date]
+    response.json(arr)
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+  response.status(204).end()
 })
 
 const PORT = 3010
